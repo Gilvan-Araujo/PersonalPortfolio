4d20dc476b2ced082166e671d46beeee2c1fa64f
Gilvan-Araujo <gilvanjunior@protonmail.com>
2021-03-31 11:33:01 -0300

    Update javascript

---
+++ apps/barbecuemeter/scripts/script.js
@@ -0,0 +1,29 @@
+function calculate() {
+    let adults = parseInt(document.getElementById("adults").value);
+    let children = parseInt(document.getElementById("children").value);
+    let duration = parseInt(document.getElementById("duration").value);
+    let meat, beer, drinks;
+
+    if (duration <= 6) {
+        meat = ((400 * adults) + (400 * (children / 2))) / 1000;
+        beer = (1200 * adults) / 1000;
+        drinks = ((1000 * adults) + (100 * children / 2)) / 1000;
+
+        showResult(meat, beer, drinks);
+    } else {
+        meat = ((650 * adults) + (650 * (children / 2))) / 1000;
+        beer = (2000 * adults) / 1000;
+        drinks = ((1500 * adults) + (100 * children / 2)) / 1000;
+
+        showResult(meat, beer, drinks);
+    }
+}
+
+function showResult(meat, beer, drinks) {
+    var result = document.getElementById("result")
+    result.innerHTML = ""
+    result.innerHTML = "<h3>Required resources</h3>"
+    result.innerHTML += `<p>Meat: ${meat} kilograms </p>`
+    result.innerHTML += `<p>Beer: ${beer} liters </p>`
+    result.innerHTML += `<p>Soda / Water: ${drinks} liters </p>`
+}
