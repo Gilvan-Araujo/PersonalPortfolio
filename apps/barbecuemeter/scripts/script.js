ada154db658c3eb727bf6e6a6e3fc68484aed7e1
Gilvan-Araujo <gilvanjunior@protonmail.com>
2021-03-31 11:58:17 -0300

    Update script.js

--- apps/barbecuemeter/scripts/script.js
+++ apps/barbecuemeter/scripts/script.js
@@ -1,21 +1,35 @@
+function validateFields(adults, children, duration) {
+    if (isNaN(adults) || isNaN(children) || isNaN(duration)) {
+        alert('Fill in the information, before calculating')
+        let result = document.getElementById("result")
+        result.innerHTML = ""
+        return false;
+    }
+    return true
+}
+
 function calculate() {
-    let adults = parseInt(document.getElementById("adults").value);
-    let children = parseInt(document.getElementById("children").value);
-    let duration = parseInt(document.getElementById("duration").value);
+    var adults = parseInt(document.getElementById("adults").value);
+    var children = parseInt(document.getElementById("children").value);
+    var duration = parseInt(document.getElementById("duration").value);
     let meat, beer, drinks;
 
-    if (duration <= 6) {
-        meat = ((400 * adults) + (400 * (children / 2))) / 1000;
-        beer = (1200 * adults) / 1000;
-        drinks = ((1000 * adults) + (100 * children / 2)) / 1000;
+    var fieldValidation = validateFields(adults, children, duration);
 
-        showResult(meat, beer, drinks);
-    } else {
-        meat = ((650 * adults) + (650 * (children / 2))) / 1000;
-        beer = (2000 * adults) / 1000;
-        drinks = ((1500 * adults) + (100 * children / 2)) / 1000;
+    if (fieldValidation) {
+        if (duration <= 6) {
+            meat = ((400 * adults) + (400 * (children / 2))) / 1000;
+            beer = (1200 * adults) / 1000;
+            drinks = ((1000 * adults) + (100 * children / 2)) / 1000;
 
-        showResult(meat, beer, drinks);
+            showResult(meat, beer, drinks);
+        } else {
+            meat = ((650 * adults) + (650 * (children / 2))) / 1000;
+            beer = (2000 * adults) / 1000;
+            drinks = ((1500 * adults) + (100 * children / 2)) / 1000;
+
+            showResult(meat, beer, drinks);
+        }
     }
 }
 
