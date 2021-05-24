2a6cf12060c32f98c30fff7a50f0500f26c66d00
Gilvan <45008443+Gilvan-Araujo@users.noreply.github.com>
2021-05-24 17:33:39 -0300

    Setup Chakra UI

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    fonts: {
+        heading: 'Open Sans',
+        body: 'Open Sans'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.900',
+                color: 'gray.50'
+            }
+        }
+    }
+})
